384b8549216be17abb02e212681636fb4bd5b2b8
dydwns <jjyyjj118@naver.com>
2025-12-27 18:32:38 +0900

    dfdf

--- backend/src/main/java/kr/co/api/backend/config/FirebaseConfig.java
+++ backend/src/main/java/kr/co/api/backend/config/FirebaseConfig.java
@@ -12,19 +12,19 @@ import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
-//
-//    @Bean
-//    public FirebaseApp firebaseApp(@Value("${firebase.service-account-path}") String path) throws Exception {
-//        try (InputStream is = new FileInputStream(path)) {
-//            FirebaseOptions options = FirebaseOptions.builder()
-//                    .setCredentials(GoogleCredentials.fromStream(is))
-//                    .build();
-//
-//            // 이미 초기화됐으면 재초기화 방지
-//            if (FirebaseApp.getApps() != null && !FirebaseApp.getApps().isEmpty()) {
-//                return FirebaseApp.getInstance();
-//            }
-//            return FirebaseApp.initializeApp(options);
-//        }
-//    }
+
+    @Bean
+    public FirebaseApp firebaseApp(@Value("${firebase.service-account-path}") String path) throws Exception {
+        try (InputStream is = new FileInputStream(path)) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(is))
+                    .build();
+
+            // 이미 초기화됐으면 재초기화 방지
+            if (FirebaseApp.getApps() != null && !FirebaseApp.getApps().isEmpty()) {
+                return FirebaseApp.getInstance();
+            }
+            return FirebaseApp.initializeApp(options);
+        }
+    }
 }
